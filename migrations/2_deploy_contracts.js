44cc03ba8eeea177e1993945528933af173e340f
xternet <majewski.post@gmail.com>
2020-09-30 12:09:35 +0200

    .

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 const Stablecoin = artifacts.require("./Stablecoin.sol");
 
 module.exports = function(deployer) {
-	deployer.deploy(Stablecoin, '0x8468b2bDCE073A157E560AA4D9CcF6dB1DB98507');
+  deployer.deploy(Stablecoin, '0x8468b2bDCE073A157E560AA4D9CcF6dB1DB98507');
 }
